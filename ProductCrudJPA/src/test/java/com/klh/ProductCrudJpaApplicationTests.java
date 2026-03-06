22c42ae7fe01cfdcdd536bffff3f00dcf9ecdbce
sphoorthy282007 <2420030510@klh.edu.in>
2026-03-06 13:55:29 +0530

    Add files via upload

---
+++ ProductCrudJPA/src/test/java/com/klh/ProductCrudJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCrudJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
